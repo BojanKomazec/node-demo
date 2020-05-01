f51d343d910093a756fcf5ff8ed43fc8cfa03e96
Bojan Komazec <bojan.komazec@gmail.com>
2020-05-01 08:04:29 +0100

    Added web request demos with packages 'request' and 'axios'.

--- modules/httpClientDemo.js
+++ modules/httpClientDemo.js
@@ -1,5 +1,61 @@
+const request = require('request');
+const axios = require('axios');
+
+/**
+ * Function which fetches http response asynchronously.
+ * It is implemented as a simple Promise wrapper around request API.
+ * @param url Request URL.
+ */
+function fetchData(url) {
+    return new Promise((resolve, reject) => {
+        if (!url) {
+            reject(ErrorFactory.createInvalidArgumentError("url"));
+        }
+
+        request(url, (error, response, body) => {
+            if (error) {
+                reject(error);
+            } else {
+                if (response) {
+                    console.log("statusCode: ", response.statusCode);
+                    if (body) {
+                        resolve(body);
+                    } else {
+                        reject(new Error("body is empty"));
+                    }
+                } else {
+                    reject(new Error("response is empty"));
+                }
+            }
+        });
+    });
+}
+
+async function requestDemo() {
+    try {
+        const responseBody = await fetchData('https://www.reddit.com/r/nodejs.json');
+        console.log(`responseBody: ${responseBody}`);
+    } catch (err) {
+        console.log(`fetchData() failed. Error: ${err}`);
+    }
+}
+
+async function axiosDemo() {
+    try {
+        // const response = await axios.get('https://www.reddit.com/r/nodejs.json');
+        console.log(`request headers: ${JSON.stringify(axios.defaults.headers)}`);
+        const response = await axios.get('https://google.com');
+        // console.log(`response: ${JSON.stringify(response.data, null, 2)}`);
+        console.log(`response headers: ${JSON.stringify(response.headers)}`);
+    } catch (err) {
+        console.log(`axios.get() failed. Error: ${err}`);
+    }
+}
+
 function demo() {
     console.log("httpClientDemo.demo()");
+    //requestDemo();
+    axiosDemo();
 }
 
 module.exports = {
